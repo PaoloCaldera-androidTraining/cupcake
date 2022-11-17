28da697c0236ecdc6578912f362ce748cfd11c3e
Paolo Caldera <paolo.caldera@keypartner.com>
2022-11-17 15:39:58 +0100

    Add correct fragment labels and implement actions

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
 
 /**
@@ -57,7 +59,8 @@ class StartFragment : Fragment() {
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
     fun orderCupcake(quantity: Int) {
-        Toast.makeText(activity, "Ordered $quantity cupcake(s)", Toast.LENGTH_SHORT).show()
+        val action = StartFragmentDirections.actionStartFragmentToFlavorFragment()
+        findNavController().navigate(action)
     }
 
     /**
