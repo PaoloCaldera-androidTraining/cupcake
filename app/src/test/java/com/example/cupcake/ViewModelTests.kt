c0fe191cee061f6a618691c23a8be8bdc249bef4
Paolo Caldera <paolo.caldera@keypartner.com>
2022-11-24 18:59:30 +0100

    Add comments

--- app/src/test/java/com/example/cupcake/ViewModelTests.kt
+++ app/src/test/java/com/example/cupcake/ViewModelTests.kt
@@ -9,6 +9,8 @@ import org.junit.Test
 
 class ViewModelTests {
 
+    // LiveData objects cannot access the main thread, which is the UI thread
+    // Explicitly state that LiveData objects do not access the main thread with the test rule
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
@@ -23,8 +25,11 @@ class ViewModelTests {
 
     fun price_twelve_cupcakes() {
         val viewModel = OrderViewModel()
+        // LiveData objects must be observed in order to record any change in its value
         viewModel.price.observeForever {}
         viewModel.setQuantity(12)
         assertEquals("$24.00", viewModel.price.value)
+
+        /* TODO: why the test method is not successful? */
     }
 }
